0d7c30fc5741408d4ab802086ef454bd358eb31d
Christopher Vestman <christophervestman@gmail.com>
2022-11-11 22:46:00 +0100

    refactor: adding python to separate folder

---
+++ python/README.md
@@ -0,0 +1,5 @@
+# Reddit - My upvoted posts Python
+
+Wanted to learn how to use the reddit api and combine it with discord webhooks.
+
+Loop through all the posts I have upvoted and saves the title, subreddit and url for the posts.
---
+++ python/toDiscord.py
@@ -0,0 +1,24 @@
+import requests
+import secrets
+import random
+import time
+
+def sendLinksToDiscord():
+    link_file = open("urls.txt", "r")
+    entries = link_file.readlines()
+    link_file.close()
+
+    number_of_posts = random.randint(10,30)
+    print(number_of_posts)
+
+    i = 0
+    while i < number_of_posts:
+        random_link = random.choice(entries)
+        send_to_discord = f"{random_link}"
+        data = {"content": send_to_discord,
+                "username": "reddit",
+                "avatar_url": f"{random_link}"
+            }
+        requests.post(secrets.Discord_Webhook, data=data)
+        time.sleep(5)
+        i += 1
